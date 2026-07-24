cdddae8836b34c79da08e52eae25be9a0bb3d2ad
Andrew Huynh <17056456+SaxyPandaBear@users.noreply.github.com>
2026-07-24 16:35:41 -0400

    try to find where API is stored. last attempt. fix filter serialization

--- graph/model/filter.go
+++ graph/model/filter.go
@@ -6,6 +6,6 @@ type Filter struct {
 	DigimonType *string  `json:"type,omitempty"`
 	Attribute   *string  `json:"attribute,omitempty"`
 	Moves       []string `json:"moves,omitempty"`
-	IsMode      *bool    `json:"isMode,omitempty"`
-	IsXAntibody *bool    `json:"isXAntibody,omitempty"`
+	IsMode      *bool    `json:"is_mode,omitempty"`
+	IsXAntibody *bool    `json:"is_x_antibody,omitempty"`
 }
